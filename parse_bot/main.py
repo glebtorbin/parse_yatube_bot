2342741f2d01acd25b8772a341b5766bc7a8410c
Глеб Торбин <torbingleb@yandex.ru>
2022-08-05 15:22:38 +0300

    создали функцию new_post, протестировали, сообщения уходят на сайт

--- parse_bot/main.py
+++ parse_bot/main.py
@@ -21,6 +21,11 @@ NEW_POST_LINK = os.getenv('NEW_POST_LINK')
 USERAGENT = fake_useragent.UserAgent().random
 SESSION = requests.Session()
 
+HEADER: dict = {
+    'Referer': AUTH_LINK,
+    'user-agent': USERAGENT
+    }
+
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
     level=logging.INFO,
@@ -29,6 +34,10 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(handler)
+
+def wake_up():
+    '''Будим бота'''
+    pass
 
 def text_accepting():
     '''Функция принимает текст для отправки на сайт'''
@@ -40,32 +49,46 @@ def acc_login():
         csrf_getting = SESSION.get(AUTH_LINK)
     except exceptions.ConnectionError:
         logger.error('Сбой подключения к AUTH_LINK')
+    logger.info('Входим в аккаунт!')
     try:
         csrf_token = csrf_getting.cookies['csrftoken']
     except exceptions.CookieError:
         message = 'Не уалось получить cookie'
         logger.error(message)
         raise exceptions.CookieError(message)
-    header = {
-    'Referer': AUTH_LINK,
-    'user-agent': USERAGENT
-    }
     data = {
         'username': 'ParseBot',
         'password': 'ZrVjhzZ5Qn6CsjG',
         'csrfmiddlewaretoken': csrf_token
     }
     try:
-        response = SESSION.post(AUTH_LINK, data=data, headers=header)
-    except ConnectionError:
+        response = SESSION.post(AUTH_LINK, data=data, headers=HEADER)
+    except exceptions.ConnectionError:
         logger.error('Сбой подключения к AUTH_LINK')
     return print(response)
     
 
-
-def new_post():
+def new_post(message):
     '''Создает новый пост'''
-    pass
+    try:
+        csrf_getting = SESSION.get(NEW_POST_LINK)
+    except exceptions.ConnectionError:
+        logger.error('Сбой подключения к NEW_POST_LINK')
+    try:
+        csrf_token = csrf_getting.cookies['csrftoken']
+    except exceptions.CookieError:
+        message = 'Не уалось получить cookie'
+        logger.error(message)
+        raise exceptions.CookieError(message)
+    data = {
+        'csrfmiddlewaretoken': csrf_token,
+        'text': message
+    }
+    try:
+        creating_post = SESSION.post(NEW_POST_LINK, data=data, headers=HEADER)
+    except exceptions.ConnectionError:
+        logger.error('Сбой подключения к AUTH_LINK')
+    return creating_post
 
 def check_tokens():
     '''Проверяет переменные окружния'''
@@ -80,3 +103,4 @@ if __name__ == '__main__':
     main()
 
 acc_login()
+new_post('q')
