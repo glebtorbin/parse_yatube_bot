e0bfcc5eae05422767461edcbc61462dae619619
Глеб Торбин <torbingleb@yandex.ru>
2022-08-08 10:48:57 +0300

    Научили бота отправлять сообщения

--- parse_bot/exceptions.py
+++ parse_bot/exceptions.py
@@ -5,3 +5,7 @@ class ConnectionError(Exception):
 class CookieError(Exception):
     '''Не плдучается подключить Куки файлы'''
     pass
+
+class ConstantError(Exception):
+    '''Отсутствует какая либо переменная'''
+    pass
--- parse_bot/main.py
+++ parse_bot/main.py
@@ -6,7 +6,7 @@ import sys
 import time
 import telegram
 from http import HTTPStatus
-from telegram.ext import CommandHandler, Updater, MessageHandler
+from telegram.ext import CommandHandler, Updater, MessageHandler, Filters
 
 import requests
 from dotenv import load_dotenv
@@ -40,19 +40,43 @@ def wake_up(update, context):
     '''Будим бота'''
     chat = update.effective_chat
     name = update.message.chat.first_name
-    button = telegram.ReplyKeyboardMarkup([['/sendmessage']], resize_keyboard=True)
+    #button = telegram.ReplyKeyboardMarkup([['/sendmessage']], resize_keyboard=True)
 
     context.bot.send_message(
         chat_id=chat.id,
         text=(f'Привет, {name}. Если хочешь сделать пост '
                'на моем сайте http://glebtorbin.pythonanywhere.com'
-               'жми /sendmessage'),
-        reply_markup=button
+               ' напиши его ниже'),
+        #reply_markup=button
     )
 
-def text_accepting():
+def send_message(update, context):
     '''Функция принимает текст для отправки на сайт'''
-    pass
+    message = update.message.text
+    try:
+        csrf_getting = SESSION.get(NEW_POST_LINK)
+    except exceptions.ConnectionError:
+        logger.error('Сбой подключения к NEW_POST_LINK')
+    try:
+        csrf_token = csrf_getting.cookies['csrftoken']
+    except exceptions.CookieError:
+        message = 'Не уалось получить cookie'
+        logger.error(message)
+        raise exceptions.CookieError(message)
+    data = {
+        'csrfmiddlewaretoken': csrf_token,
+        'text': message
+    }
+    logger.info('Отправляем сообщение...')
+    update.message.reply_text('Отправляем сообщение...')
+    try:
+        creating_post = SESSION.post(NEW_POST_LINK, data=data, headers=HEADER)
+        update.message.reply_text('Сообщение успешно отправлено!')
+        logger.info('Сообщение успешно отправлено!')
+    except exceptions.ConnectionError:
+        update.message.reply_text('Сбой подключения, попробуйте позже')
+        logger.error('Сбой подключения к AUTH_LINK')
+    return creating_post
 
 def acc_login():
     '''Входит в аккаунт'''
@@ -78,46 +102,26 @@ def acc_login():
         logger.error('Сбой подключения к AUTH_LINK')
     return print(response)
     
-
-def new_post(message):
-    '''Создает новый пост'''
-    try:
-        csrf_getting = SESSION.get(NEW_POST_LINK)
-    except exceptions.ConnectionError:
-        logger.error('Сбой подключения к NEW_POST_LINK')
-    try:
-        csrf_token = csrf_getting.cookies['csrftoken']
-    except exceptions.CookieError:
-        message = 'Не уалось получить cookie'
-        logger.error(message)
-        raise exceptions.CookieError(message)
-    data = {
-        'csrfmiddlewaretoken': csrf_token,
-        'text': message
-    }
-    logger.info('Отправляем сообщение...')
-    try:
-        creating_post = SESSION.post(NEW_POST_LINK, data=data, headers=HEADER)
-    except exceptions.ConnectionError:
-        logger.error('Сбой подключения к AUTH_LINK')
-    return creating_post
-
 def check_tokens():
     '''Проверяет переменные окружния'''
     return all([TELEGRAM_TOKEN, AUTH_LINK, NEW_POST_LINK, USERAGENT])
 
 def main():
     '''Основная функция описывающия логику и работу бота'''
-    updater = Updater(token=TELEGRAM_TOKEN)
+    if not check_tokens():
+        msg = 'Проверьте все переменные окружения'
+        logger.critical(msg)
+        raise exceptions.ConstantError(msg)
+    
+    while True:
+        updater = Updater(token=TELEGRAM_TOKEN)
 
-    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-    acc_login()
-    new_post('s')
-    updater.start_polling()
-    updater.idle()
+        updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+        updater.dispatcher.add_handler(MessageHandler(Filters.text, send_message))
+        acc_login()
+        updater.start_polling()
+        updater.idle()
     
     
-
-
 if __name__ == '__main__':
     main()
